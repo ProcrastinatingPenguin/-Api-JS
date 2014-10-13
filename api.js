ff5ba1f4d5e5068834829286cb960cbd87a4d0ce
Joshua Page <joshuapage@me.com>
2014-10-13 13:49:11 +0100

    First Version Upload
    getJSON function made

---
+++ api.js
@@ -0,0 +1,20 @@
+var getJSON = function (url, successHandler, errorHandler) {
+    var xhr = typeof XMLHttpRequest != 'undefined' ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+    xhr.open('get', url, true);
+
+    xhr.onreadystatechange = function () {
+        var status;
+        var data;
+
+        if (xhr.readyState == 4) { // `DONE`
+            status = xhr.status;
+            if (status == 200) {
+                data = JSON.parse(xhr.responseText);
+                successHandler && successHandler(data);
+            } else {
+                errorHandler && errorHandler(status);
+            }
+        }
+    };
+    xhr.send();
+};
